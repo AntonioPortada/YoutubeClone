fb8dda832f0ccfec3b20e18fbdd3f53c84f77f0d
AntonioPortada <antonio_portada25@hotmail.com>
2024-11-03 17:26:19 -0600

    models vid-6

---
+++ YoutubeClone/YoutubeClone/Model/PlaylistItemModel.swift
@@ -0,0 +1,28 @@
+//
+//  PlaylistItemModel.swift
+//  YoutubeClone
+//
+//  Created by Antonio Portada on 03/11/24.
+//
+
+import Foundation
+
+
+struct PlaylistItemsModel: Decodable{
+    let kind: String
+    let etag: String
+    let items: [PlaylistItems]
+    let pageInfo : PageInfo
+    
+    struct PlaylistItems: Decodable{
+        let kind : String
+        let etag : String
+        let id : String
+        let snippet : VideoModel.Item.Snippet
+    }
+    
+    struct PageInfo: Decodable{
+        let totalResults: Int
+        let resultsPerPage: Int
+    }
+}
